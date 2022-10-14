28734ac835b789eb5a235dd31bb70b2b5361fe81
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2022-10-14 07:53:20 -0400

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name='Arise',
+	description='Zero-K',
+	version='v0.0.0',
+	shortname='Arise',
+	mutator='1',
+	game='Zero-K',
+	shortGame='ZK',
+	modtype=1,
+	depend = {
+		[[rapid://zk:stable]],
+	},
+}
